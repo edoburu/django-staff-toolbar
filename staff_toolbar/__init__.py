5b233009c18edefbab6fdd5ba86121dfdca0afd3
Diederik van der Boor <vdboor@edoburu.nl>
2016-08-07 18:24:24 +0200

    Move methods to `conf.py`, added `toolbar_link()`

--- staff_toolbar/__init__.py
+++ staff_toolbar/__init__.py
@@ -1,50 +1,10 @@
-# following PEP 440
+from .conf import toolbar_title, toolbar_item, toolbar_literal, toolbar_link
 
-__version__ = "1.0.1"
+__version__ = "1.0.1"  # following PEP 440
 
 __all__ = (
     'toolbar_title',
     'toolbar_item',
     'toolbar_literal',
+    'toolbar_link',
 )
-
-
-def toolbar_title(title):
-    """
-    Define a title to be included in the toolbar.
-    """
-    return LazyToolbarItem('staff_toolbar.items.Title', title)
-
-
-def toolbar_literal(title):
-    """
-    Define a literal text to be included in the toolbar.
-    """
-    return LazyToolbarItem('staff_toolbar.items.Literal', title)
-
-
-def toolbar_item(callable_path, *args, **kwargs):
-    """
-    Defining a toolbar item in the settings that also received arguments.
-    It won't be loaded until the toolbar is actually rendered.
-    """
-    return LazyToolbarItem(callable_path, *args, **kwargs)
-
-
-class LazyToolbarItem(object):
-    """
-    Internal mechanism to support lazy-loaded toolbar items in the settings.
-    """
-    def __init__(self, import_path, *args, **kwargs):
-        self.import_path = import_path
-        self.args = args
-        self.kwargs = kwargs
-        self.real_instance = None
-
-    def __call__(self, request, context):
-        if self.real_instance is None:
-            # Init on demand.
-            from staff_toolbar.loading import load_toolbar_item
-            self.real_instance = load_toolbar_item(self.import_path, *self.args, **self.kwargs)
-
-        return self.real_instance(request, context)
---
+++ staff_toolbar/conf.py
@@ -0,0 +1,48 @@
+
+
+def toolbar_title(title):
+    """
+    Define a title to be included in the toolbar.
+    """
+    return LazyToolbarItem('staff_toolbar.items.Title', title)
+
+
+def toolbar_literal(title):
+    """
+    Define a literal text to be included in the toolbar.
+    """
+    return LazyToolbarItem('staff_toolbar.items.Literal', title)
+
+
+def toolbar_item(callable_path, *args, **kwargs):
+    """
+    Defining a toolbar item in the settings that also received arguments.
+    It won't be loaded until the toolbar is actually rendered.
+    """
+    return LazyToolbarItem(callable_path, *args, **kwargs)
+
+
+def toolbar_link(url, title):
+    """
+    Define a link to be included in the toolbar.
+    """
+    return LazyToolbarItem('staff_toolbar.items.Link', url=url, title=title)
+
+
+class LazyToolbarItem(object):
+    """
+    Internal mechanism to support lazy-loaded toolbar items in the settings.
+    """
+    def __init__(self, import_path, *args, **kwargs):
+        self.import_path = import_path
+        self.args = args
+        self.kwargs = kwargs
+        self.real_instance = None
+
+    def __call__(self, request, context):
+        if self.real_instance is None:
+            # Init on demand.
+            from staff_toolbar.loading import load_toolbar_item
+            self.real_instance = load_toolbar_item(self.import_path, *self.args, **self.kwargs)
+
+        return self.real_instance(request, context)
